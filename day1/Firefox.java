3cb6ff82c72581fe95be343175bb3583a8e548a7
Pavithra500162 <139791836+Pavithra500162@users.noreply.github.com>
2023-07-17 21:39:50 +0530

    Add files via upload
    Day 1 Assignments

---
+++ day1/Firefox.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float version=100.2f;
+String name="firefox";
+boolean visible=true;
+int year=1998;
+char logo='m';
+System.out.println("Version of fire fox: "+version+"\n"+"Name of the Browser: "+name+"\n"+"Visiblity: "+visible+"\n"+"Year: "+year+"\n"+"Logo :"+logo);
+	}
+
+}
---
+++ day1/HelloSelenium.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class HelloSelenium {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+
+//datatype variable=value
+      int price=100000;        
+      String brandName="Tata"; //camalCase        
+      //String hold collection of characters
+      char logo='T';        
+      float fuel=12.45f;    
+      String fuelCapcity="12.45lt";
+      double modelVariant=456.2343565757;
+      boolean isNew=false;        
+      long phNo=673528208366722l;
+      
+      System.out.println("logo is : "+logo);        
+      System.out.println(logo);        
+      System.out.println("The brand name of the Car :"+brandName);
+      System.out.println(price);
+      System.out.println(fuel);
+      System.out.println(modelVariant +": "+isNew);
+	}
+
+}
